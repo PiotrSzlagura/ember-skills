d9ffea6f2e778d083c6234004880852c521d3da1
PiotrSzlagura <pszlagura@outlook.com>
2016-02-25 22:29:33 +0100

    Fixed current state presentation

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,14 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    updateSkill(skill, step) {
-      let end = !skill.get(`step${step}`);
-      skill.set(`step${step}`, end);
-      console.log(`${skill} step${step} ` + end);
+    updateSkill(skill) {
+      //let end = !skill.get(`step${step}`);
+      //skill.set(`step${step}`, end);
+      //console.log(`${skill} step${step} ` + end);
       //console.log(`Step: ${step}, current: ${currentState} end state: ${endState}`);
       skill.save();
+      //alert(skill);
+    },
 
-
+    removeSkill(skill) {
+      skill.destroyRecord();
     }
   }
+
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -6,18 +6,28 @@ export default Ember.Route.extend({
     },
 
     actions: {
-    addSkill(skillName) {
-      let newSkill = this.store.createRecord('skill', {
-        name: skillName,
-        step1: true,
-        step2: false,
-        step3: false
-      });
+      addSkill(skillName) {
+        let newSkill = this.store.createRecord('skill', {
+          name: skillName,
+          step1: true,
+          step2: false,
+          step3: false
+        });
 
-      newSkill.save();
 
-      this.controller.set('skillName', '');
-    },
+        newSkill.save();
+
+        this.controller.set('skillName', '');
+      },
+      updateSkill(skill, step) {
+        let end = !skill.get(`step${step}`);
+        skill.set(`step${step}`, end);
+        console.log(`${skill} step${step} ` + end);
+        //console.log(`Step: ${step}, current: ${currentState} end state: ${endState}`);
+        skill.save();
+        alert(skill);
+
+      }
   }
 
 });
